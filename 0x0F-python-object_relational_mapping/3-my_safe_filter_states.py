ee450b0cf7f5c1ffda16b4b58e55ce08ffc49155
yassine4real <yssnabbou2002@gmail.com>
2024-06-26 18:21:39 +0100

    fixing bugs

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states 
-table of hbtn_0e_0_usa where name matches the argument but this time that is safe from MySQL injections!"""
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument but this time that
+is safe from MySQL injections!
+"""
 import sys
 import MySQLdb
+
 if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_searched = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -15,11 +20,14 @@ if __name__ == "__main__":
         passwd=mysql_password,
         db=database_name
     )
+
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
     cursor.execute(query, (state_searched,))
     states = cursor.fetchall()
+
     for state in states:
         print(state)
+
     cursor.close()
     db.close()
