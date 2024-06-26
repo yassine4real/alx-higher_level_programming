b87c99963397c8fd3654e2291833bdc9f28c83da
yassine4real <yssnabbou2002@gmail.com>
2024-06-26 16:46:31 +0100

    fixing bugs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values
-   in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
 import sys
 import MySQLdb
 if __name__ == "__main__":
